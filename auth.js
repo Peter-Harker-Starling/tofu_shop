775728b3fba7955db980681b96f69a8eecaed6ad
harkerstarling-bit <petera9liu@gmail.com>
2026-02-11 17:37:11 +0800

    first

---
+++ auth.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+  const token = req.cookies.admin_token;
+
+  if (!token) {
+    return res.redirect('/users/login');
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.admin = decoded; // 把老闆的資訊掛在 req 上
+    next();
+  } catch (err) {
+    // 只要 token 出錯（過期或被竄改），清空並重新登入
+    res.clearCookie('admin_token');
+    return res.redirect('/users/login');
+  }
+}
+
+module.exports = auth;
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const supabase = require('./supabaseClient');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(methodOverride('_method')); //讓伺服器檢查網址是否有?_method=DELETE或?_method=PATCH
+app.use(express.urlencoded({ extended: true })); //讓Express能夠讀懂HTML(Form)送來的資料
+app.use(cookieParser());
+app.use(express.static('public')); //在Express設定「靜態檔案」
+
+app.get('/', async (req, res) => {
+    try {
+        const { data, error } = await supabase.from('admin').select('username');
+        if (error) throw error;
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    };
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Running on ${PORT}`);
+});
